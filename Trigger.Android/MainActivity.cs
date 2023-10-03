fa5e5a61513e72a5ea0c8f01a29e4b0fabb8e85a
milkfalling <hen85618@gmail.com>
2023-10-03 11:31:05 +0800

    no message

---
+++ Trigger.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Content;
+
+namespace Trigger.Droid
+{
+    [Activity(Label = "Trigger", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            string url = "SeeingIGo://Notify_SIG_of_Navigation_By_Site_Id?site_id=88600001";  // &location_latitude=23.69759851641522&location_longitude=120.52590174734716
+            Intent intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse(url));
+            StartActivity(intent);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Trigger.iOS/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+using UIKit;
+
+namespace Trigger.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            // Triggering the deep link.
+            var url = new NSUrl("SeeingIGo://Notify_SIG_of_Navigation_By_Site_Id?site_id=88600001"); // You can add more parameters if needed.
+            if (UIApplication.SharedApplication.CanOpenUrl(url))
+            {
+                UIApplication.SharedApplication.OpenUrl(url);
+            }
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
